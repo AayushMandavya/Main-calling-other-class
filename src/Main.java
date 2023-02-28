fe71cf5fc04edfaf0ddae638d55a7f72d09cfeba
aayushmandavya <mandavyaaayush98@gmail.com>
2023-02-28 10:22:33 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String x=new String();
+        shape object=new shape() ;
+        System.out.println(object.width);
+        System.out.println(object.length);
+        object.area();
+
+    }
+}
---
+++ src/shape.java
@@ -0,0 +1,7 @@
+public class shape {
+     int width=10;
+    int length=50;
+    public void area(){
+        System.out.println(width*length);
+    }
+}
